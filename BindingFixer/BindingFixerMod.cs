d65916fb3db64210d3369678857e5751330c52df
Nicolas Gnyra <nicolas@gnyra.com>
2021-09-30 18:47:24 -0400

    Initial commit

---
+++ BindingFixer/BindingFixerMod.cs
@@ -0,0 +1,82 @@
+﻿using MelonLoader;
+using SG.XR.InputHandling;
+using System;
+using System.Linq;
+using UnhollowerRuntimeLib;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+[assembly: MelonInfo(typeof(BindingFixer.BindingFixerMod), "Binding Fixer", "1.0.0", "nicoco007")]
+[assembly: MelonGame("Schell Games", "UntilYouFall")]
+namespace BindingFixer
+{
+    public class BindingFixerMod : MelonMod
+    {
+        private static readonly string[] kShouldBeAnyInputSourceButtons =
+        {
+            "/actions/default/in/RotateLeft",
+            "/actions/default/in/RotateRight",
+            "/actions/default/in/DashForward",
+            "/actions/default/in/DashBack",
+        };
+
+        private static readonly string[] kShouldBeAnyInputSourceAxes =
+        {
+            "/actions/default/in/Movement",
+        };
+
+        private readonly UnityAction<Scene, LoadSceneMode> _sceneLoadedHandler;
+
+        private bool _processed;
+
+        public BindingFixerMod()
+        {
+            _sceneLoadedHandler = new Action<Scene, LoadSceneMode>(OnSceneLoaded);
+        }
+
+        public override void OnApplicationStart()
+        {
+            SceneManager.add_sceneLoaded(_sceneLoadedHandler);
+            base.OnApplicationStart();
+        }
+
+        public override void OnApplicationQuit()
+        {
+            SceneManager.remove_sceneLoaded(_sceneLoadedHandler);
+            base.OnApplicationQuit();
+        }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (scene.name != "TitleScreen" || _processed)
+            {
+                return;
+            }
+
+            foreach (OpenVRButton openVrButton in Resources.FindObjectsOfTypeAll(Il2CppType.Of<OpenVRButton>()).Select(b => b.Cast<OpenVRButton>()))
+            {
+                string fullPath = openVrButton.button.axis.fullPath;
+
+                if (kShouldBeAnyInputSourceButtons.Contains(fullPath))
+                {
+                    MelonLogger.Msg($"Fixing '{fullPath}'");
+                    openVrButton.button.node = Valve.VR.SteamVR_Input_Sources.Any;
+                }
+            }
+
+            foreach (OpenVRAxis openVrAxis in Resources.FindObjectsOfTypeAll(Il2CppType.Of<OpenVRAxis>()).Select(b => b.Cast<OpenVRAxis>()))
+            {
+                string fullPath = openVrAxis.axis.fullPath;
+
+                if (kShouldBeAnyInputSourceAxes.Contains(fullPath))
+                {
+                    MelonLogger.Msg($"Fixing '{fullPath}'");
+                    openVrAxis.node = Valve.VR.SteamVR_Input_Sources.Any;
+                }
+            }
+
+            _processed = true;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Until You Fall Binding Fixer
+![Build Status](https://img.shields.io/github/workflow/status/nicoco007/UntilYouFallBindingFixer/Build?style=flat-square)
+
+Small mod that allows movement, dash, and turn bindings to be remapped to either controller. Works with [MelonLoader](https://melonwiki.xyz/) v0.3.0 and v0.4.3. Note that you need to change the bindings in your SteamVR controller settings for this to have any effect.
+
+[Download the latest build](https://nightly.link/nicoco007/UntilYouFallBindingFixer/workflows/build/main/BindingFixer.zip).
